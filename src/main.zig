254479b18bb229cedd7047f3a6939b0c5c24326d
philipsoitu <philip.soitu@gmail.com>
2025-08-19 19:24:36 -0400

    num of devices

--- src/main.zig
+++ src/main.zig
@@ -7,13 +7,15 @@ pub fn main() !void {
     std.debug.print("libfreenect init test\n", .{});
 
     var ctx: ?*c.freenect_context = null;
-
     if (c.freenect_init(&ctx, null) < 0) {
         std.debug.print("freenext init failed\n", .{});
         return;
     }
 
-    std.debug.print("freenect_init succeeded: {any}\n", .{ctx});
+    defer _ = c.freenect_shutdown(ctx);
 
-    _ = c.freenect_shutdown(ctx);
+    const num_devices = c.freenect_num_devices(ctx);
+    std.debug.print("num_devices: {d}\n", .{num_devices});
+
+    std.debug.print("freenect_init succeeded: {any}\n", .{ctx});
 }
