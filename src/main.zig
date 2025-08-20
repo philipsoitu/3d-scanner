7aecf0f8c26f31669f547541d284d34769ef6b4a
philipsoitu <philip.soitu@gmail.com>
2025-08-20 01:13:29 -0400

    wip

--- src/main.zig
+++ src/main.zig
@@ -6,8 +6,5 @@ pub fn main() !void {
     var k = try kinect.Kinect.init();
     defer k.shutdown();
 
-    while (true) {
-        std.Thread.sleep(100_000);
-        std.debug.print(".", .{});
-    }
+    while (true) {}
 }
