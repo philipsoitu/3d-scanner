92071f77403da323c5ff31017c5d592186c7ac35
philipsoitu <philip.soitu@gmail.com>
2025-08-23 19:33:13 -0400

    first test + sync updates

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 rsync -avz phil@192.168.2.100:/home/phil/3d-scanner/kinect_output/ /home/phil/code/3d-scanner/kinect_output/
+rsync -avz phil@192.168.2.100:/home/phil/3d-scanner/pointcloud.ply /home/phil/code/3d-scanner/pointcloud.ply
