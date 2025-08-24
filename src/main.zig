8008b03da90fc7cc173ce3b0d8abdef7fd78e28f
philipsoitu <philip.soitu@gmail.com>
2025-08-24 14:28:59 -0400

    multithreaded start

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,5 @@
 const std = @import("std");
-const kinect = @import("kinect.zig");
-const Frame = @import("frame.zig").Frame;
-const point_cloud = @import("point_cloud.zig");
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
-
-    var frame = Frame.init(640, 480);
-
-    var starting_state = kinect.KinectState{
-        .depth_captured = false,
-        .rgb_captured = false,
-        .frame = &frame,
-    };
-
-    var k: kinect.Kinect = try kinect.Kinect.init(&starting_state);
-    try k.runLoop();
-    defer k.shutdown();
-
-    const points = try point_cloud.frameToPoints(allocator, &frame);
-    defer allocator.free(points);
-
-    try point_cloud.writePLY(points, "pointcloud.ply");
+    std.debug.print("yo\n", .{});
 }
