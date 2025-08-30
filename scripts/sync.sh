a43d9876cd401848dc3772b2f7b4cfbd7edcf963
philipsoitu <philip.soitu@gmail.com>
2025-08-30 13:48:33 -0400

    updated path

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync -avz phil@192.168.2.100:/home/phil/3d-scanner/kinect_output/ /home/phil/code/3d-scanner/kinect_output/
+rsync -avz phil@192.168.2.100:/home/phil/kinect-scanner/kinect_output/ /home/phil/code/kinect-scanner/kinect_output/
