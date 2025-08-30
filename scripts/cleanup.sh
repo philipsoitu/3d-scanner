809c5f62c2a69fb75a9e1bc8e394b205b4e0ee1f
philipsoitu <philip.soitu@gmail.com>
2025-08-30 11:53:06 -0400

    scripts

---
+++ scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+rm -rf ../kinect_output/rgb/*
+rm -rf ../kinect_output/depth/*
--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
 rsync -avz phil@192.168.2.100:/home/phil/3d-scanner/kinect_output/ /home/phil/code/3d-scanner/kinect_output/
-rsync -avz phil@192.168.2.100:/home/phil/3d-scanner/pointcloud.ply /home/phil/code/3d-scanner/pointcloud.ply
