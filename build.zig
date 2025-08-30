0fd5e2713912e01e3510fa1637ea9160f7c06efa
philipsoitu <philip.soitu@gmail.com>
2025-08-30 19:16:19 -0400

    renamed binary

--- build.zig
+++ build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "_3d_scanner",
+        .name = "kinect-scanner",
         .root_module = exe_mod,
     });
 
