45e2d087756af739e9a18226cdf18ff62afcc352
philipsoitu <philip.soitu@gmail.com>
2025-09-10 18:33:26 -0400

    intrinsics: https://www.researchgate.net/publication/305108995

--- src/config.zig
+++ src/config.zig
@@ -5,12 +5,12 @@ pub const RGB_BUFFER_SIZE = 640 * 480 * 3;
 pub const DEPTH_BUFFER_SIZE = 640 * 480 * 2;
 pub const DEPTH_BUFFER_SIZE_U16 = 640 * 480;
 
-pub const DEPTH_FX: f64 = 594.21;
-pub const DEPTH_FY: f64 = 591.04;
-pub const DEPTH_CX: f64 = 339.5;
-pub const DEPTH_CY: f64 = 242.7;
+pub const DEPTH_FX: f64 = 585.6;
+pub const DEPTH_FY: f64 = 585.6;
+pub const DEPTH_CX: f64 = 316;
+pub const DEPTH_CY: f64 = 247.6;
 
-pub const RGB_FX: f64 = 529.2;
-pub const RGB_FY: f64 = 525.6;
-pub const RGB_CX: f64 = 329.0;
-pub const RGB_CY: f64 = 247.6;
+pub const RGB_FX: f64 = 524;
+pub const RGB_FY: f64 = 524;
+pub const RGB_CX: f64 = 316.7;
+pub const RGB_CY: f64 = 238.5;
