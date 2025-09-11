fa8a677388cf76cb375754dcb2507cddcfc5dcd5
philipsoitu <philip.soitu@gmail.com>
2025-09-11 19:30:11 -0400

    12x speedup holy shit i love zig multithreading!

--- src/generate.zig
+++ src/generate.zig
@@ -12,8 +12,35 @@ pub fn run(allocator: std.mem.Allocator) !void {
     defer allocator.free(pairs);
     std.debug.print("pairs: {}\n", .{pairs.len});
 
+    // get thread counts
+    const cpu_count = try std.Thread.getCpuCount();
+    const chunk_size = (pairs.len + cpu_count - 1) / cpu_count;
+
+    const threads = try allocator.alloc(std.Thread, cpu_count);
+    defer allocator.free(threads);
+
+    // initialize threads
+    for (threads, 0..) |*t, t_id| {
+        const start = t_id * chunk_size;
+        if (start >= pairs.len) break;
+        const end = @min(start + chunk_size, pairs.len);
+        const slice = pairs[start..end];
+
+        t.* = try std.Thread.spawn(.{}, worker, .{ t_id, slice, allocator });
+    }
+
+    // start threads
+    for (threads) |*t| {
+        t.*.join();
+    }
+}
+
+fn worker(thread_id: usize, pairs: []const FramePair, allocator: std.mem.Allocator) !void {
     for (pairs, 0..pairs.len) |pair, i| {
-        std.debug.print("{d} depth: {}, rgb: {}\n", .{ i + 1, pair.depth_timestamp, pair.rgb_timestamp });
+        std.debug.print(
+            "thread:{d:0>2} frame:{d:0>3} depth: {}, rgb: {}\n",
+            .{ thread_id, i + 1, pair.depth_timestamp, pair.rgb_timestamp },
+        );
 
         // filename
         var filename_buf: [256]u8 = undefined;
@@ -28,11 +55,5 @@ pub fn run(allocator: std.mem.Allocator) !void {
         defer allocator.free(point);
 
         try point_cloud.writePLY(point, filename);
-
-        //TODO: Write save to disk funcction
-        //        _ = pointcloud;
     }
-
-    //TODO: Run ICP code to generate final pointcloud
-
 }
